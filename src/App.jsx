d3da58c151091bd6bbdd3a2641246f0e6a30d6ff
Shauqi <ahmadshauqi88@gmail.com>
2025-03-11 12:25:15 +0800

    add UpdatePostModal & postsSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Col, Container, Row } from "react-bootstrap";
 import IconButton from "./components/IconButton";
 import ProfileHeader from "./components/ProfileHeader";
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { PROFILE_DATA } from "./data";
 import ImageGrid from "./components/ImageGrid";
+import AddPostModal from "./components/AddPostModal";
 
 export const ProfileContext = createContext(null);
 
 export default function App() {
+
+  const [showModal, setShowModal] = useState(false);
+  const closeModal = () => setShowModal(false);
+  const openModal = () => setShowModal(true);
+
   return (
     <ProfileContext.Provider value={PROFILE_DATA}>
       <Row>
@@ -22,7 +28,7 @@ export default function App() {
           <IconButton className="bi bi-film" />
           <IconButton className="bi bi-chat" />
           <IconButton className="bi bi-balloon-heart" />
-          <IconButton className="bi bi-plus-circle" />
+          <IconButton className="bi bi-plus-circle" onClick={openModal} />
           <IconButton className="bi bi-person-square" />
           <IconButton className="bi bi-list" isBottom />
 
@@ -32,6 +38,7 @@ export default function App() {
           <Container>
             <ProfileHeader />
             <ImageGrid />
+            <AddPostModal show={showModal} handleClose={closeModal} />
           </Container>
 
         </Col>
--- src/data.js
+++ src/data.js
@@ -1,56 +1,53 @@
 export const PROFILE_DATA = {
     image: `https://pbs.twimg.com/profile_images/1839294511824121858/pol0zyxM_400x400.jpg`,
     name: "Shauqi",
-    posts_no: 465,
-    followers: 1723,
-    following: 9,
-    subheader: "Sigma School | Tech Education & Services",
-    account_type: "Education",
+    posts_no: 8,
+    followers: 10.9,
+    following: 999,
+    subheader: "Will work for Royal Canin | Tech Cat",
+    account_type: "Parody",
     description: `
-      🚨 Upskill yourself, learn to code
-      👨‍💻 Develop full stack web & mobile apps
-      🤝🏻 Build real world projects
-      💯 Free if you don’t land a tech job
+      ⚛️ Turning coffee into code (oh wait, its Ramadhan..) | React noob |✨404: free time not found✨
       `,
-    link: "linktr.ee/joinsigma",
+    link: "https://www.linkedin.com/in/ahmad-shauqi-bin-mohd",
     posts: [
         {
-            image: "https://picsum.photos/id/123/500/500",
+            image: "https://i.pinimg.com/1200x/58/3a/42/583a426c84dbff9431691a54ffe38355.jpg",
             description: "Post 1 description",
             date: new Date().toISOString(),
             likes: 15,
             comments: 5,
         },
         {
-            image: "https://picsum.photos/id/124/500/500",
+            image: "https://i.pinimg.com/1200x/d7/36/ae/d736ae8116a1523e5ab9d0c04bc2bc93.jpg",
             description: "Post 2 description",
             date: new Date().toISOString(),
             likes: 30,
             comments: 10,
         },
         {
-            image: "https://picsum.photos/id/125/500/500",
+            image: "https://i.pinimg.com/1200x/d4/b1/67/d4b167320f798276ecb362fe8083129c.jpg",
             description: "Post 2 description",
             date: new Date().toISOString(),
             likes: 30,
             comments: 10,
         },
         {
-            image: "https://picsum.photos/id/126/500/500",
+            image: "https://i.pinimg.com/1200x/7c/ae/c3/7caec3a746fbab6094edad143e702204.jpg",
             description: "Post 2 description",
             date: new Date().toISOString(),
             likes: 30,
             comments: 10,
         },
         {
-            image: "https://picsum.photos/id/127/500/500",
+            image: "https://i.pinimg.com/1200x/70/90/f2/7090f2c633fa7ab7c7439bb33c053d1d.jpg",
             description: "Post 2 description",
             date: new Date().toISOString(),
             likes: 30,
             comments: 10,
         },
         {
-            image: "https://picsum.photos/id/128/500/500",
+            image: "https://i.pinimg.com/1200x/43/c8/7c/43c87c94cd74a161fa9fa6c4e03be472.jpg",
             description: "Post 2 description",
             date: new Date().toISOString(),
             likes: 30,
